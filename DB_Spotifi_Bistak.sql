d1c38b3868ddeaf0d26a8493c7892a6a4b58cce1
UnaiS <unai.soutoto@elorrieta-errekamari.com>
2024-04-24 13:20:55 +0200

    bistak eginda

--- DB_Spotifi_Bistak.sql
+++ DB_Spotifi_Bistak.sql
@@ -9,17 +9,20 @@ select p.Izenburua, p.Sorrera_data, b.Erabiltzailea as 'Sortzailea'
 from Playlist p inner join Bezeroa b using(ID_Bezeroa)
 where b.Erabiltzailea = 'sophia_fernandez';
 
--- Musikari konkretu baten Albumak ikusteko.
-drop view if exists erakutsiMusikariarenAlbumak;
-create view erakutsiMusikariarenAlbumak as 
-select a.Izenburua, a.Urtea, a.Generoa , m.Izen_Artistikoa
-from Album a inner join Musikaria m using(ID_Musikaria)
-where m.Izen_Artistikoa = 'Estopa';
+-- Musikarien abestiak zenbat erreprodukzio daukaten ikusteko.
+drop view if exists musikaDeskubritu;
+create view musikaDeskubritu as 
+select m.Izen_Artistikoa as 'Musikaria', count(e.ID_Audio) as 'Erreprodukzioak'
+from Musikaria m inner join Album a using(ID_Musikaria)
+					inner join Abestia ab using(ID_Album)
+						inner join Audio au using(ID_Audio)
+							inner join Erreprodukzioak e using(ID_Audio)
+group by m.Izen_Artistikoa;
 
--- Zein erabiltzaile entzun duen The Wild Project #1
-drop view if exists TWPEntzumena;
-create view TWPEntzumena as 
-select a.Izena, a.Iraupena, a.Mota, b.Erabiltzailea as 'Nortzuk entzun dute'
-from Audio a inner join Erreprodukzioak e using(ID_Audio)
-				inner join Bezeroa b using(ID_Bezeroa)
-where a.Izena = 'The Wild Proyect Podcast #1';
+-- Podcasterren podcastak zenbat erreprodukzio daukaten ikusteko.
+drop view if exists podcastDeskubritu;
+create view podcastDeskubritu as 
+select p.Izen_artistikoa as 'Podcasterra', count(e.ID_Audio) as 'Erreprodukzioak'
+from Podcaster p inner join Podcast po using(ID_Podcaster)
+					inner join Erreprodukzioak e using(ID_Audio)
+group by p.Izen_artistikoa;
